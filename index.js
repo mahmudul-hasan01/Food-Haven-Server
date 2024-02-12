1b73097ebda24fb88007c2a7d57a519cf3b009e9
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-02-12 13:09:33 +0600

    verify admin

--- index.js
+++ index.js
@@ -58,6 +58,18 @@ async function run() {
       })
     }
 
+    // verify admin 
+    const verifyAdmin =async (req, res, next) => {
+      const email = req.decoded.email
+      const query = {email: email}
+      const user = await users.findOne(query)
+      const isAdmin = user?.role === 'admin'
+      if(!isAdmin){
+        return res.status(403).send({ massege: 'forbidden access' })
+      }
+      next()
+    }
+
     // Users
     app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
       const result = await users.find().toArray()
