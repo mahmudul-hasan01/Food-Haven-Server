8b02af8fe47b4baebda02d6be4f68ff55cb5a25f
mahmudul-hasan01 <mahamudul2018@gmail.com>
2024-02-25 11:28:21 +0600

    delete menu item

--- index.js
+++ index.js
@@ -129,6 +129,13 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await menu.deleteOne(query)
+      res.send(result)
+    })
+
     //review
     app.get('/reviews', async (req, res) => {
       const result = await reviews.find().toArray()
